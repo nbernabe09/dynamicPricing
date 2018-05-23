510f8976baf68d0fb9bcfa53089cfdb27f9613a9
Adam Husain <medaman@gmail.com>
2018-05-23 12:29:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connection.connect(error => {
   if (error) {
     console.log(error);
   }
-  console.log('connected');
+  console.log('connectedsdadsadsa');
 });
 
 app.get('/api/data', (req, res) => {
